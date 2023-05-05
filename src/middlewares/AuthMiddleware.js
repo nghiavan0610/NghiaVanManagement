50ca32240e0207af610b83c54fed29ed4b8a423e
BenQ <nghiavan0610@gmail.com>
2023-05-05 18:44:26 +0700

    Implement AuthMiddleware

---
+++ src/middlewares/AuthMiddleware.js
@@ -0,0 +1,40 @@
+const { ApiError } = require('../helpers/ErrorHandler');
+const { verifyAccessToken } = require('../helpers/Token');
+const redisClient = require('../configs/init.redis');
+
+const User = require('../db/models/user');
+
+const authenticateToken = async (req, res, next) => {
+    try {
+        const { authorization } = req.headers;
+
+        if (!authorization || !authorization.startsWith('Bearer')) {
+            throw new ApiError(401, 'Invalid authorization');
+        }
+
+        const accessToken = authorization.split(' ')[1];
+        if (!accessToken || accessToken === 'undefined') {
+            throw new ApiError(401, 'Access Token was not found');
+        }
+
+        const payload = await verifyAccessToken(accessToken);
+
+        const user = await User.findById(payload.userId);
+        if (!user) {
+            throw new ApiError(401, `Account does not exist`);
+        }
+        const storedAccessToken = await redisClient.get(`accessToken:${user.id}`);
+        if (accessToken !== storedAccessToken) {
+            throw new ApiError(401, 'Access token has been revoked');
+        }
+        req.user = user;
+        next();
+    } catch (err) {
+        if (err.name === 'TokenExpiredError') {
+            next(new ApiError(401, 'Access token has been timed out'));
+        }
+        next(err);
+    }
+};
+
+module.exports = { authenticateToken };
--- src/routes/v1/user.js
+++ src/routes/v1/user.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../../app/controllers/UserController');
+const { authenticateToken } = require('../../middlewares/AuthMiddleware');
 
-router.get('/:userSlug', userController.getUserBySlug);
+router.get('/:userSlug', authenticateToken, userController.getUserBySlug);
 
 module.exports = router;
